be4947210c5f1d4914be19a4fcc9b0f52233a0cb
Daniel Lee <jhyunglee.cs@gmail.com>
2023-03-12 15:25:29 -0400

    fixed internal server error

--- flask_shodan.py
+++ flask_shodan.py
@@ -1,24 +1,26 @@
+#! usr/bin python3
+
 from flask import Flask, render_template, request
 from shodan_funcAPI import search_org
+import shodan 
+
 
 app = Flask(__name__)
 
-# Working route function 
 @app.route('/')
 def index():
-    #return render_template('index.html')
-    return "This is Working"
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    org = 'Drexel University'
+    port = request.form['port']
+    results = search_org(org, port)
+    return render_template('result.html', results=results)
+
 
 # Running on Local Host 
 app.run(host="0.0.0.0", port=80)
 
-#@app.route('/search', methods=['POST'])
-#def search():
- #   org = request.form['org']
-  #  results = search_org(org)
-   # return render_template('search.html', org=org, results=results)
-
-#if __name__ == '__main__':
-    #app.run()
 
 
--- shodan_funcAPI.py
+++ shodan_funcAPI.py
@@ -2,12 +2,13 @@
 
 import shodan
 
-api_key = '864UqpknfcTN9HWpkX8Kz5bzzaTiWNWc' # Use your API key here
+api_key = '864UqpknfcTN9HWpkX8Kz5bzzaTiWNWc' #jl3993 API Key. Use yours here
 
-def search_org(org):
+def search_org(org, port):
     api = shodan.Shodan(api_key)
     try:
-        results = api.search('org:"{}"'.format(org))
+        query = 'org:"{}" port:{}'.format(org, port)
+        results = api.search(query)
         return results['matches']
     except shodan.APIError as e:
         print('Error: {}'.format(e))
